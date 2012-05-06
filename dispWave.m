2e4a26f9e1eae9bd2b9e67d1ceb15fe4a0354a2b
Yiliang Xiong <wlxiong@gmail.com>
2012-05-06 11:55:15 +0800

    visualize the waves 2

--- dispWave.m
+++ dispWave.m
@@ -1,14 +1,7 @@
-figure
-nbin = floor(sqrt(size(wave,1)));
-[n, xout] = hist(wave, nbin);
-[c i] = max(n);
-threshold = (xout(i) + xout(i+1))/2.0;
-axis([0.15 .2 0 160000])
-
-figure
-hist(wave(2:160000,2), 1000);
-axis([0 .3 0 160000])
-
-figure
-hist(wave(3:160000,3), 1000);
-axis([0 .1 0 160000])
+function dispWave(waves, stimulusTimes, actionTimes)
+% data visualization
+figure; hold on
+plot(waves)
+legend('15','10','9')
+plot(stimulusTimes, ones(length(stimulusTimes),1),'*k')
+plot(actionTimes, ones(length(actionTimes),1),'ok')
