6fe3c2f3472aeca1da4c1b078dd149088b02fcbf
Yiliang Xiong <wlxiong@gmail.com>
2012-05-06 13:06:06 +0800

    plot .mat data directly

--- dispWave.m
+++ dispWave.m
@@ -1,7 +1,10 @@
-function dispWave(waves, stimulusTimes, actionTimes)
+function dispWave(matfilename)
 % data visualization
+% load the data
+load(matfilename)
+% plot the waves
 figure; hold on
-plot(waves)
+plot(fWaves)
 legend('15','10','9')
 plot(stimulusTimes, ones(length(stimulusTimes),1),'*k')
 plot(actionTimes, ones(length(actionTimes),1),'ok')
