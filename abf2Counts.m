09c82b27377e7028d31198733c6a802b43a36a7f
Yiliang Xiong <wlxiong@gmail.com>
2012-05-06 13:04:37 +0800

    automatically choose the right channels

--- abf2Counts.m
+++ abf2Counts.m
@@ -1,14 +1,15 @@
-function [responseTimes, stimulusTimes, actionTimes, waves, timeunit, meta] = abf2Counts(abfFilename)
-
-% import data
-[waves,timeunit,meta] = abfload2(abfFilename);
+function [responseTimes, stimulusTimes, actionTimes, fWaves] = abf2Counts(waves, meta)
+% convert abf waves to counts
 % get the absolute values of the waves
 waves = abs(waves);
 % calculate the average values of each 10 points
 n = 10;
-actionWave   = filterWave(waves(:,1), n);
-probeWave    = filterWave(waves(:,2), n);
-stimulusWave = filterWave(waves(:,3), n);
+actionChInd   = find(cellfun(@length, strfind(meta.recChNames, '9')));
+probeChInd    = find(cellfun(@length, strfind(meta.recChNames, '10')));
+stimulusChInd = find(cellfun(@length, strfind(meta.recChNames, '15')));
+actionWave   = filterWave(waves(:,actionChInd), n);
+probeWave    = filterWave(waves(:,probeChInd), n);
+stimulusWave = filterWave(waves(:,stimulusChInd), n);
 fWaves = [stimulusWave,probeWave,actionWave];
 % extract pusles from each wave
 actionPulses   = findPulseInterval(actionWave);
