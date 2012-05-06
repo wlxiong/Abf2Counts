732282f65daef3a40632213c74dc3507d25ff587
Yiliang Xiong <wlxiong@gmail.com>
2012-05-06 13:06:54 +0800

    output .mat and .csv along with the original .abf

--- batchRun.m
+++ batchRun.m
@@ -1,13 +1,26 @@
-function [abflist] = batchRun(apath)
+function [timelist, abflist] = batchRun(apath)
 % batch run abf2Counts
 apath = fileparts(apath);
 abfpath = [apath '/*.abf'];
 abflist = rdir(abfpath);
-
+timelist = cell(length(abflist),1);
 for p = 1:length(abflist)
+    fprintf('\n *** %d/%d *** \n', p, length(abflist))
+	% import data
+	fprintf('\n [Importing]: %s\n\n', abflist(p).name)
+	[waves,timeunit,meta] = abfload2(abflist(p).name);
+	% process data
 	fprintf('\n [Processing]: %s\n\n', abflist(p).name)
-	[responseTimes, stimulusTimes, actionTimes, waves, timeunit, meta] = abf2Counts(abflist(p).name);
+	[responseTimes, stimulusTimes, actionTimes, fWaves] = abf2Counts(waves,meta);
+	timelist{p} = responseTimes;
+	% export results
 	[pathstr, name, ext] = fileparts(abflist(p).name);
-	save([pathstr, name, '.mat'], responseTimes, stimulusTimes, actionTimes, waves, timeunit, meta);
-	xlswrite([pathstr, name, '.xls'], responseTimes, stimulusTimes, actionTimes);
+	matfilename = fullfile(pathstr, [name, '.mat']);
+	csvfilename = fullfile(pathstr, [name, '.csv']);
+	save(matfilename, 'responseTimes', 'stimulusTimes', 'actionTimes', 'fWaves');
+	csvwrite(csvfilename, [responseTimes, stimulusTimes, actionTimes]);
+	% display summary
+	fprintf(' no. of response: %d\n', length(responseTimes));
+	fprintf(' mean response time: %.2f ms\n', mean(responseTimes));
+	fprintf(' std. response time: %.2f ms\n',  std(responseTimes));
 end
