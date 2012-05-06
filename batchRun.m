ace57a4d75cb8f2755cc8a0bf9c45e14d3b6eec0
Yiliang Xiong <wlxiong@gmail.com>
2012-05-06 11:10:41 +0800

    fix iteration of struct in MATLAB

--- batchRun.m
+++ batchRun.m
@@ -1,13 +1,13 @@
 function [abflist] = batchRun(apath)
 % batch run abf2Counts
-cd(apath);
-apath = pwd;
+apath = fileparts(apath);
 abfpath = [apath '/*.abf'];
 abflist = rdir(abfpath);
 
 for p = 1:length(abflist)
-	[responseTimes, stimulusTimes, actionTimes, waves, timeunit, meta] = abf2Counts(abflist.name(p));
-	[pathstr, name, ext] = fileparts(abflist.name(p));
+	fprintf('\n [Processing]: %s\n\n', abflist(p).name)
+	[responseTimes, stimulusTimes, actionTimes, waves, timeunit, meta] = abf2Counts(abflist(p).name);
+	[pathstr, name, ext] = fileparts(abflist(p).name);
 	save([pathstr, name, '.mat'], responseTimes, stimulusTimes, actionTimes, waves, timeunit, meta);
 	xlswrite([pathstr, name, '.xls'], responseTimes, stimulusTimes, actionTimes);
 end
